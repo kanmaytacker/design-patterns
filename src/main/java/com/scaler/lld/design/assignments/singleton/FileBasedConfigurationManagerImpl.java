72b1d626b7f2a78cf5cd2e70d7938c51fe6ade5d
Milan Jayan <milanjayan.lamr.007@gmail.com>
2023-09-15 18:59:04 +0530

    singleton assignment complete

--- src/main/java/com/scaler/lld/design/assignments/singleton/FileBasedConfigurationManagerImpl.java
+++ src/main/java/com/scaler/lld/design/assignments/singleton/FileBasedConfigurationManagerImpl.java
@@ -2,49 +2,77 @@ package com.scaler.lld.design.assignments.singleton;
 
 public class FileBasedConfigurationManagerImpl extends FileBasedConfigurationManager {
 
+    private static FileBasedConfigurationManager instance = null;
+
+    private  FileBasedConfigurationManagerImpl() {}
+
     @Override
     public String getConfiguration(String key) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getConfiguration'");
+        return getProperties().getProperty(key);
     }
 
     @Override
     public <T> T getConfiguration(String key, Class<T> type) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getConfiguration'");
+        String value = getProperties().getProperty(key);
+        if(value == null) {
+            return null;
+        }
+        if(type.isAssignableFrom(String.class)) {
+            return (T) value;
+        }
+        else if(type.isAssignableFrom(Integer.class)) {
+            return (T) Integer.valueOf(value);
+        }
+        else if(type.isAssignableFrom(Double.class)) {
+            return (T) Double.valueOf(value);
+        }
+        else if(type.isAssignableFrom(Float.class)) {
+            return (T) Float.valueOf(value);
+        }
+        else if(type.isAssignableFrom(Boolean.class)) {
+            return (T) Boolean.valueOf(value);
+        }
+        else {
+            return null;
+        }
     }
 
     @Override
     public void setConfiguration(String key, String value) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setConfiguration'");
+        getProperties().setProperty(key, value);
     }
 
     @Override
     public <T> void setConfiguration(String key, T value) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setConfiguration'");
+        if(value instanceof String) {
+            String v = (String)value;
+            getProperties().setProperty(key, v);
+        }
     }
 
     @Override
     public void removeConfiguration(String key) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeConfiguration'");
+        getProperties().remove(key);
     }
 
     @Override
     public void clear() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'clear'");
+        getProperties().clear();
     }
 
     public static FileBasedConfigurationManager getInstance() {
-        // TODO Auto-generated method stub
-        return null;
+        if(instance == null) {
+            synchronized (FileBasedConfigurationManagerImpl.class) {
+                if(instance == null) {
+                    instance = new FileBasedConfigurationManagerImpl();
+                }
+            }
+        }
+        return instance;
     }
 
     public static void resetInstance() {
-        // TODO Auto-generated method stub
+        instance = null;
     }
 
 }
