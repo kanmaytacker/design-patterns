ebe859cf5a7abd2a4c9fda9511c7ee929965902c
Pramod Patil <pramodnitkmca@gmail.com>
2023-09-18 19:38:40 +0530

    Builder pattern with and without fields duplication

--- src/main/java/com/scaler/lld/design/assignments/builder/DatabaseConfigurationBuilderWithoutFieldsDuplication.java
+++ src/main/java/com/scaler/lld/design/assignments/builder/DatabaseConfigurationBuilderWithoutFieldsDuplication.java
@@ -1,7 +1,7 @@
 package com.scaler.lld.design.assignments.builder;
 
 @WithBuilder
-public class DatabaseConfigurationBuilder {
+public class DatabaseConfigurationBuilderWithoutFieldsDuplication {
     private String databaseUrl;
     private String username;
     private String password;
@@ -9,51 +9,48 @@ public class DatabaseConfigurationBuilder {
     private boolean enableCache;
     private boolean isReadOnly;
 
-    private DatabaseConfigurationBuilder() {
+    private DatabaseConfigurationBuilderWithoutFieldsDuplication() {
     }
 
     public static class Builder {
-        private String databaseUrl;
-        private String username;
-        private String password;
-        private int maxConnections;
-        private boolean enableCache;
-        private boolean isReadOnly;
-
+        private DatabaseConfigurationBuilderWithoutFieldsDuplication instance;
         public Builder() {
+            instance = new DatabaseConfigurationBuilderWithoutFieldsDuplication();
         }
 
         public DatabaseConfiguration build() {
-            return new DatabaseConfiguration(databaseUrl, username, password, maxConnections, enableCache, isReadOnly);
+            //return new object instead of "instance" variable due to immutability advantage.
+            return new DatabaseConfiguration(instance.databaseUrl, instance.username, instance.password,
+                    instance.maxConnections, instance.enableCache, instance.isReadOnly);
         }
 
         public Builder setDatabaseUrl(String databaseUrl) {
-            this.databaseUrl = databaseUrl;
+            instance.databaseUrl = databaseUrl;
             return this;
         }
 
         public Builder setUsername(String username) {
-            this.username = username;
+            instance.username = username;
             return this;
         }
 
         public Builder setPassword(String password) {
-            this.password = password;
+            instance.password = password;
             return this;
         }
 
         public Builder setMaxConnections(int maxConnections) {
-            this.maxConnections = maxConnections;
+            instance.maxConnections = maxConnections;
             return this;
         }
 
         public Builder setEnableCache(boolean enableCache) {
-            this.enableCache = enableCache;
+            instance.enableCache = enableCache;
             return this;
         }
 
         public Builder setReadOnly(boolean readOnly) {
-            isReadOnly = readOnly;
+            instance.isReadOnly = readOnly;
             return this;
         }
     }
