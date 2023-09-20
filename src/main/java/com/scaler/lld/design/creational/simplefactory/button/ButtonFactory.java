3e41a58fa078898ce6b0ee96644e3d9aff0acba5
shailesh <85shailesh@gmail.com>
2023-09-20 09:22:56 +0530

    implemented to avoid parameter explosion in the simple factory

--- src/main/java/com/scaler/lld/design/creational/simplefactory/button/ButtonFactory.java
+++ src/main/java/com/scaler/lld/design/creational/simplefactory/button/ButtonFactory.java
@@ -1,16 +1,66 @@
 package com.scaler.lld.design.creational.simplefactory.button;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
 public class ButtonFactory {
 
-    // Step 3 - Create a static factory method
-    public static Button createButton(ScreenSize screenSize, Double border, Double radius, Double length) {
-        switch (screenSize) {
-            case PHONE:
-            case TABLET: return new RoundButton(border, radius);
-            case DESKTOP: return new SquareButton(border, length);
+    private ScreenSize screenSize;
+    private Double border;
+    private Double radius;
+    private Double length;
+
+    private ButtonFactory(){
+
+    }
+    public static Builder getBuilder(){
+        return new Builder();
+    }
+    @Setter
+    @Getter
+    static class Builder{
+        private ScreenSize screenSize;
+        private Double border;
+        private Double radius;
+        private Double length;
+
+        public Builder setScreenSize(ScreenSize screenSize) {
+            this.screenSize = screenSize;
+            return this;
         }
 
-        throw new IllegalArgumentException("Invalid type: " + screenSize);
+        public Builder setBorder(Double border) {
+            this.border = border;
+            return this;
+        }
+
+        public Builder setRadius(Double radius) {
+            this.radius = radius;
+            return this;
+        }
+
+        public Builder setLength(Double length) {
+            this.length = length;
+            return this;
+        }
+
+        public  Button build(){
+           return createButton(this);
+       }
+    }
+
+    // Step 3 - Create a static factory method
+    public static Button createButton(Builder build) {
+        switch (build.getScreenSize()) {
+            case PHONE:
+            case TABLET: return new RoundButton(build.getBorder(), build.getRadius());
+            case DESKTOP: return new SquareButton(build.getBorder(), build.getLength());
+        }
+
+        throw new IllegalArgumentException("Invalid type: " + build.getScreenSize());
 
     }
 }
--- src/test/java/com/scaler/lld/design/creational/simplefactory/button/ButtonTest.java
+++ src/test/java/com/scaler/lld/design/creational/simplefactory/button/ButtonTest.java
@@ -9,10 +9,10 @@ public class ButtonTest {
 
     @Test
     public void testRoundButton() {
-        Button button = ButtonFactory.createButton(
+       /* Button button = ButtonFactory.createButton(
                 ScreenSize.PHONE, 10.0, 1.0, null
-        );
-
+        );*/
+        Button button = ButtonFactory.getBuilder().setScreenSize(ScreenSize.PHONE).setRadius(10.0).setBorder(1.0).build();
         assertTrue(button instanceof RoundButton,
                 "If the screen size is of a phone, the btn should be round"
         );
@@ -20,10 +20,10 @@ public class ButtonTest {
 
     @Test
     public void testSquareButton() {
-        Button button = ButtonFactory.createButton(
-                ScreenSize.DESKTOP, 10.0, null, 10.0
-        );
-
+//        Button button = ButtonFactory.createButton(
+//                ScreenSize.DESKTOP, 10.0, null, 10.0
+//        );
+        Button button = ButtonFactory.getBuilder().setScreenSize(ScreenSize.DESKTOP).setLength(10.0).setBorder(10.0).build();
         assertTrue(button instanceof SquareButton,
                 "If the screen size is of a desktop, the btn should be square"
         );
