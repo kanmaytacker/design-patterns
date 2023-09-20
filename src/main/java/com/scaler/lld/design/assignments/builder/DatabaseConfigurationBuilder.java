f86070078e7a86d0442b6ba450e4d2cb998c50b7
Amandeep Saluja <saluja.amandeep9@gmail.com>
2023-09-20 17:23:13 +0530

    first assignment

--- src/main/java/com/scaler/lld/design/assignments/builder/DatabaseConfigurationBuilder.java
+++ src/main/java/com/scaler/lld/design/assignments/builder/DatabaseConfigurationBuilder.java
@@ -1,6 +1,78 @@
 package com.scaler.lld.design.assignments.builder;
 
+import lombok.Getter;
+
 @WithBuilder
+@Getter
 public class DatabaseConfigurationBuilder {
 
+    private String databaseUrl;
+    private String username;
+    private String password;
+    private int maxConnections;
+    private boolean enableCache;
+    private boolean isReadOnly;
+
+    private DatabaseConfigurationBuilder(){
+    }
+
+    public static DBConfigBuilder builder(){
+        return new DBConfigBuilder();
+    }
+
+    public static class DBConfigBuilder {
+        public String databaseUrl;
+        private String username;
+        private String password;
+        private int maxConnections;
+        private boolean enableCache;
+        private boolean isReadOnly;
+
+        // Creating fluent interfaces for setter
+        public DBConfigBuilder setDatabaseUrl(String databaseUrl){
+            this.databaseUrl = databaseUrl;
+            return this;
+        }
+        public DBConfigBuilder setUsername(String username){
+            this.username = username;
+            return this;
+        }
+        public DBConfigBuilder setPassword(String password){
+            this.password = password;
+            return this;
+        }
+        public DBConfigBuilder setMaxConnections(int maxConnections){
+            this.maxConnections = maxConnections;
+            return this;
+        }
+        public DBConfigBuilder setEnableCache(boolean enableCache){
+            this.enableCache = enableCache;
+            return this;
+        }
+        public DBConfigBuilder setIsReadOnly(boolean isReadOnly){
+            this.isReadOnly = isReadOnly;
+            return this;
+        }
+
+        public DatabaseConfigurationBuilder build(){
+//            if (!isValid()){
+//                throw new IllegalArgumentException("Ivalid Database Configuration");
+//            }
+            DatabaseConfigurationBuilder databaseConfigurationBuilder = new DatabaseConfigurationBuilder();
+            databaseConfigurationBuilder.databaseUrl = this.databaseUrl;
+            databaseConfigurationBuilder.username = this.username;
+            databaseConfigurationBuilder.password = this.password;
+            databaseConfigurationBuilder.maxConnections = this.maxConnections;
+            databaseConfigurationBuilder.enableCache = this.enableCache;
+            databaseConfigurationBuilder.isReadOnly = this.isReadOnly;
+            return databaseConfigurationBuilder;
+        }
+
+//        public Boolean isValid(){
+//            if (this.databaseUrl == null){
+//                return false;
+//            }
+//            return true;
+//        }
+    }
 }
--- src/main/java/com/scaler/lld/design/assignments/singleton/FileBasedConfigurationManagerImpl.java
+++ src/main/java/com/scaler/lld/design/assignments/singleton/FileBasedConfigurationManagerImpl.java
@@ -1,11 +1,23 @@
 package com.scaler.lld.design.assignments.singleton;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FileBasedConfigurationManagerImpl extends FileBasedConfigurationManager {
+
+    private static FileBasedConfigurationManager INSTANCE = null;
+    public Map<String, String> config = new HashMap<>();
+//    public Map<String, > configDiffType = new HashMap<>();
 
     @Override
     public String getConfiguration(String key) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getConfiguration'");
+//        // TODO Auto-generated method stub
+//        throw new UnsupportedOperationException("Unimplemented method 'getConfiguration'");
+        String rvalue = "stringValue";
+        if (config.containsKey(key)){
+            rvalue = config.get(key);
+        }
+        return rvalue;
     }
 
     @Override
@@ -16,35 +28,45 @@ public class FileBasedConfigurationManagerImpl extends FileBasedConfigurationMan
 
     @Override
     public void setConfiguration(String key, String value) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setConfiguration'");
+//        // TODO Auto-generated method stub
+//        throw new UnsupportedOperationException("Unimplemented method 'setConfiguration'");
+        config.put(key, value);
     }
 
     @Override
     public <T> void setConfiguration(String key, T value) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setConfiguration'");
+
     }
 
     @Override
     public void removeConfiguration(String key) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeConfiguration'");
+//        // TODO Auto-generated method stub
+//        throw new UnsupportedOperationException("Unimplemented method 'removeConfiguration'");
+        config.put(key, null);
     }
 
     @Override
     public void clear() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'clear'");
+//        // TODO Auto-generated method stub
+//        throw new UnsupportedOperationException("Unimplemented method 'clear'");
+        config.clear();
     }
 
     public static FileBasedConfigurationManager getInstance() {
-        // TODO Auto-generated method stub
-        return null;
+        if (INSTANCE == null){
+            synchronized (FileBasedConfigurationManagerImpl.class){
+                if (INSTANCE == null){
+                    INSTANCE = new FileBasedConfigurationManagerImpl();
+                }
+            }
+        }
+        return INSTANCE;
     }
 
     public static void resetInstance() {
-        // TODO Auto-generated method stub
+        INSTANCE = null;
     }
 
 }
