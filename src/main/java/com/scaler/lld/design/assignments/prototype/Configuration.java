bc5bddc3eb398d6009b5854d3dc0351bf6d706c9
mohammadmehditalib <mohammadmehditalib@gmail.com>
2023-10-02 04:35:31 +0530

    implementing prototype

--- src/main/java/com/scaler/lld/design/assignments/prototype/Configuration.java
+++ src/main/java/com/scaler/lld/design/assignments/prototype/Configuration.java
@@ -1,6 +1,6 @@
 package com.scaler.lld.design.assignments.prototype;
 
-public class Configuration {
+public class Configuration implements ClonableObject {
     private String themeColor;
     private Boolean autoSave;
     private String language;
@@ -9,7 +9,8 @@ public class Configuration {
     private String fontFamily;
     private ConfigurationType type;
 
-    public Configuration(String themeColor, Boolean autoSave, String language, Boolean darkMode, Integer fontSize, String fontFamily, ConfigurationType type) {
+    public Configuration(String themeColor, Boolean autoSave, String language, Boolean darkMode, Integer fontSize,
+            String fontFamily, ConfigurationType type) {
         this.themeColor = themeColor;
         this.autoSave = autoSave;
         this.language = language;
@@ -17,6 +18,21 @@ public class Configuration {
         this.fontSize = fontSize;
         this.fontFamily = fontFamily;
         this.type = type;
+    }
+
+    public Configuration() {
+
+    }
+
+    public Configuration Configuration(Configuration ref) {
+        ref.themeColor = this.themeColor;
+        ref.autoSave = this.autoSave;
+        ref.language = this.language;
+        ref.darkMode = this.darkMode;
+        ref.fontSize = this.fontSize;
+        ref.fontFamily = this.fontFamily;
+        ref.type = this.type;
+        return ref;
     }
 
     public String getThemeColor() {
@@ -46,4 +62,12 @@ public class Configuration {
     public ConfigurationType getType() {
         return type;
     }
+
+    @Override
+    public Object cloneObject() {
+        Configuration ref = new Configuration();
+        return Configuration(ref);
+
+    }
+
 }
