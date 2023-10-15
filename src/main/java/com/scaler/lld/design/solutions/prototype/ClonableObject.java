392b9ed3097fc0a57b2bb3495f32f55037a8dbb3
Onkar Marbhal <marbhal37@gmail.com>
2023-10-15 16:15:17 +0530

    feat: prototype and registry pattern solutions added.

---
+++ src/main/java/com/scaler/lld/design/solutions/prototype/ClonableObject.java
@@ -0,0 +1,5 @@
+package com.scaler.lld.design.solutions.prototype;
+
+public interface ClonableObject<T> {
+    T cloneObject();
+}
---
+++ src/main/java/com/scaler/lld/design/solutions/prototype/ConfigurationType.java
@@ -0,0 +1,8 @@
+package com.scaler.lld.design.solutions.prototype;
+
+public enum ConfigurationType {
+    BASIC,
+    ADVANCED,
+    CUSTOM,
+    DEFAULT
+}
